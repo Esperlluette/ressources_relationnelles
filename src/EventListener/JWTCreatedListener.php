32efdaae050f3a0ebd21c29d6995bbde43aa70c0
esperlluette <Janl.mannessier@gmail.com>
2025-04-26 08:12:30 +0000

    claims ok

---
+++ src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+
+class JWTCreatedListener
+{
+    /**
+     * Cette méthode est appelée juste avant la création du token.
+     * de cette manière, je peux ajouter des informations supplémentaires au token.
+     */
+    public function onJWTCreated(JWTCreatedEvent $event): void
+    {
+        $payload = $event->getData();
+        $user    = $event->getUser();
+
+        // --- J'informe intelisense que $user est de type AppUser ---
+        /** @var \App\Entity\AppUser $user */
+
+        $payload["username"] = $user->getName();
+        $payload['email']    = $user->getUserIdentifier();
+
+        $event->setData($payload);
+    }
+}
